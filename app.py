5ae91c06c81d81cc6f73f202ec582f8fb187c333
legionchaos-sg <wjzkev@gmail.com>
2026-03-28 18:35:47 +0800

    Correct comment formatting in app.py
    Fix indentation and correct comment formatting.

--- app.py
+++ app.py
@@ -364,7 +364,7 @@ with tab5:
             base = 850
         else: 
             base = 500
-     """
+    """
         """
         # Get inflation adjustment from your SG Economy Engine
         inf_adj = 1 + (sg_econ.get('inf_val', 1.2) / 100)
