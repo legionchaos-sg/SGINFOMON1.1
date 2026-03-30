faa33118838451291bd911b3094f9e130520c8eb
legionchaos-sg <wjzkev@gmail.com>
2026-03-30 21:43:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -529,9 +529,8 @@ with tab2:
 
     #-----------------HDB National Resale  
     with st.expander("📊 **National HDB Resale Sentiments**", expanded=False):
- 
-        if not df_debug.empty:
-            stats = calculate_national_averages(df_debug)
+         
+          calculate_national_averages(df_debug)
             
             if stats is not None:
                 st.markdown("### 📊 2026 National Average Resale Prices")
