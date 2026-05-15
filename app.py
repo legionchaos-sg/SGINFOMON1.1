ba1c632c2c48410cd1bedb2d9c5ec4966502eacf
legionchaos-sg <wjzkev@gmail.com>
2026-05-15 21:32:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -824,7 +824,7 @@ with tab1:
             """, unsafe_allow_html=True)    
 
     # 6. FUEL MONITOR SECTION
-    f_avg, f_brands, f_timing, f_savings = fetch_fuel_logic(user_car_min_grade="95", user_current_grade="98")
+    f_avg, f_brands, f_timing, f_savings = fetch_fuel_logic(brent_now, brent_3d_ago)
 
     with st.expander("⛽ Average Fuel Prices (S$/Litre)", expanded=True):
         # --- Price Cards Section ---
